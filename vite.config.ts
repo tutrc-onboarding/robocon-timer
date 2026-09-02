c03eb2c5aebb848589d3845fe35f03ffb0a4ad9c
Rin Iwai <me@eyrin.jp>
2026-09-02 17:51:07 +0900

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/robocon-timer/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
